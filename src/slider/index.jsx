1d6bf70502f93afa39273c234e0a177457772810
Yenice <perezyenice@gmail.com>
2023-07-20 11:34:52 -0300

    style scrollerft

--- src/slider/index.jsx
+++ src/slider/index.jsx
@@ -1,39 +1,69 @@
 import { useRef } from 'react';
 import './style.css'
 
-
 const Slider = ({ children }) => {
     const sliderContentRef = useRef(null);
     const startX = useRef(null);
     const scrollLeft = useRef(null);
 
-    const onHandleClikNext = () => {
+    const onHandleClickNext = () => {
         sliderContentRef.current.scrollLeft += sliderContentRef.current.children[0].offsetWidth;
-    }
+    };
 
-const onHandleClikPrevious = () => {
-    sliderContentRef.current.scrollLeft -= sliderContentRef.current.children[0].offsetWidth;
-}
+    const onHandleClickPrevious = () => {
+        sliderContentRef.current.scrollLeft -= sliderContentRef.current.children[0].offsetWidth;
+    };
+
+    const onHandleMouseDown = (event) => {
+        startX.current = event.pageX - sliderContentRef.current.offsetLeft;
+        scrollLeft.current = sliderContentRef.current.scrollLeft;
+    };
+
+    const onHandleMouseLeave = (event) => {};
+
+    const onHandleMouseUp = (event) => {};
+
+    const onHandleMouseMove = (event) => {
+        event.preventDefault();
+        const x = event.clientX - sliderContentRef.current.offsetLeft;
+        const walk = (x - startX.current) * 3;
+        sliderContentRef.current.scrollLeft = scrollLeft.current - walk;
+    };
+
+    const onHandleTouchStart = (event) => {
+        startX.current = event.touches[0].clientX - sliderContentRef.current.offsetLeft;
+        scrollLeft.current = sliderContentRef.current.scrollLeft;
+    };
+
+    const onHandleTouchEnd = (event) => {};
+
+    const onHandleTouchMove = (event) => {
+        event.preventDefault();
+        const x = event.touches[0].clientX - sliderContentRef.current.offsetLeft;
+        const walk = (x - startX.current) * 3;
+        sliderContentRef.current.scrollLeft = scrollLeft.current - walk;
+    };
 
     return (
         <div className="slider">
-            <button onClick={onHandleClikPrevious} type='button' className='previousButton'><span>&lt;</span></button>
-            <button onClick={onHandleClikNext} type='button' className='nextButton'><span>&gt;</span></button>
+            <button onClick={onHandleClickPrevious} type='button' className='previousButton'><span>&lt;</span></button>
+            <button onClick={onHandleClickNext} type='button' className='nextButton'><span>&gt;</span></button>
 
-            <div 
+            <div
                 ref={sliderContentRef}
-                 className='sliderContent'>
-                onMouseDown={}
-                onMouseLeave={}
-                onMouseUp={}
-                onMouseMove={}
-                onTouchEnd={}
-                onTouchMove={}
-
+                className='sliderContent'
+                onMouseDown={onHandleMouseDown}
+                onMouseLeave={onHandleMouseLeave}
+                onMouseUp={onHandleMouseUp}
+                onMouseMove={onHandleMouseMove}
+                onTouchStart={onHandleTouchStart}
+                onTouchEnd={onHandleTouchEnd}
+                onTouchMove={onHandleTouchMove}
+            >
                 {children}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Slider;
